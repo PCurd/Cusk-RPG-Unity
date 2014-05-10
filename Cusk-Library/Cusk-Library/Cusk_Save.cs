08465a93f6550ff76d1a43ab315bccc17516a5e9
Peter Curd <peter@pcurd.co.uk>
2014-05-10 21:59:34 +0100

    Initial Solution for Cusk_Library

---
+++ Cusk-Library/Cusk-Library/Cusk_Save.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cusk_Library
+{
+    public class Cusk_Save
+    {
+    }
+}
