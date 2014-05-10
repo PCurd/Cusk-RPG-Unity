6235c95ece989b6a2fe274987617d84eba64df11
Peter Curd <peter@pcurd.co.uk>
2014-05-10 22:28:59 +0100

    First tests Added GameState

--- Cusk-Library/Cusk-Library/GameState.cs
+++ Cusk-Library/Cusk-Library/GameState.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Cusk_Library
 {
-    public class Cusk_Save
+    public class GameState
     {
     }
 }
---
+++ Cusk-Library/Cusk-Library/Save.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cusk_Library
+{
+    public class Save
+    {
+        public Save()
+        {
+
+        }
+
+
+
+        public bool Serialize(GameState gameState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
